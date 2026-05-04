74d91c227acf83d8142ebfc8a52e8c73b474417a
Facu Mazza <fmazza@mobydigital.com>
2026-05-04 14:27:00 -0300

    feat: agregar modelos de entidades Paciente, Profesional y Turno

---
+++ src/main/java/com/clinica/turnos/model/Paciente.java
@@ -0,0 +1,30 @@
+package com.clinica.turnos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String dni;
+
+    private String email;
+}
